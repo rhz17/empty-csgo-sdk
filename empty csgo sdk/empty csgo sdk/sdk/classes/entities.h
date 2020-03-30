0373179a6fa2866970a73ca45132a99bfd405331
ReignG <46023876+ReignG@users.noreply.github.com>
2020-03-30 16:18:47 -0300

    Update entities.h

--- empty csgo sdk/empty csgo sdk/sdk/classes/entities.h
+++ empty csgo sdk/empty csgo sdk/sdk/classes/entities.h
@@ -41,8 +41,9 @@ namespace sdk {
 			fl_unblockable_by_player = (1 << 30)
 		};
 
-		class base_entity {
+		class base_general {
 		public:
+			// DT_BaseEntity
 			get_netvar(OwnerHandle(), unsigned, "DT_BaseEntity", "m_hOwnerEntity");
 			get_netvar(Spotted(), bool, "DT_BaseEntity", "m_bSpotted");
 			get_netvar(FovTime(), float, "DT_BaseEntity", "m_flShadowCastDistance");
@@ -52,10 +53,8 @@ namespace sdk {
 			get_netvar(VecMins(), sdk::math::vector_3d, "DT_BaseEntity", "m_Collision", "m_vecMins");
 			get_netvar(VecMaxs(), sdk::math::vector_3d, "DT_BaseEntity", "m_Collision", "m_vecMaxs");
 			get_netvar(ModelIndex(), unsigned, "DT_BaseEntity", "m_nModelIndex");
-		};
 
-		class base_player {
-		public:
+			// DT_BasePlayer
 			get_netvar(ViewOffset(), sdk::math::vector_3d, "DT_BasePlayer", "localdata", "m_vecViewOffset[0]");
 			get_netvar(ViewModel(), std::int32_t, "DT_BasePlayer", "m_hViewModel[0]");
 			get_netvar(PunchAngle(), sdk::math::vector_3d, "DT_BasePlayer", "localdata", "m_Local", "m_viewPunchAngle");
@@ -70,10 +69,8 @@ namespace sdk {
 			get_netvar(TickBase(), std::int32_t, "DT_BasePlayer", "localdata", "m_nTickBase");
 			get_netvar(DuckAmount(), float, "DT_BasePlayer", "m_flDuckAmount");
 			get_netvar(DuckSpeed(), float, "DT_BasePlayer", "m_flDuckSpeed");
-		};
 
-		class cs_player {
-		public:
+			// DT_CSPlayer
 			get_netvar(HasDefuser(), bool, "DT_CSPlayer", "m_bHasDefuser");
 			get_netvar(GunGameImmunity(), bool, "DT_CSPlayer", "m_bGunGameImmunity");
 			get_netvar(ShotsFired(), std::int32_t, "DT_CSPlayer", "cslocaldata", "m_iShotsFired");
@@ -91,11 +88,6 @@ namespace sdk {
 			get_netvar(HasHeavyArmor(), bool, "DT_CSPlayer", "m_bHasHeavyArmor");
 			get_netvar(HostageAlive(), bool, "DT_CSPlayer", "m_bHostageAlive");
 			get_netvar(Ragdoll(), std::int32_t, "DT_CSPlayer", "m_hRagdoll")
-		};
-
-		class base_general : public base_entity, public base_player, public cs_player {
-		public:
-
 		};
 	};
 };
